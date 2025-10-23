d915bf06d24b592378b4eabf63a040bda53803be
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-10-23 08:26:30 +0700

    menambahkan teks untuk kolom pertama yang berisi nama prodi  dengan mengambil id yang ada di res yang sudah dibuat tadi

--- app/src/main/java/com/example/praktikum4/UIPertama.kt
+++ app/src/main/java/com/example/praktikum4/UIPertama.kt
@@ -3,9 +3,11 @@ package com.example.praktikum4
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 
@@ -13,6 +15,9 @@ import androidx.compose.ui.unit.dp
 fun ActivitasPertama(modifier: Modifier){
     Column(modifier= Modifier.padding(top = 100.dp)
         .fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally) {  }
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = stringResource(id=R.string.prodi),)
+    }
 }
 
