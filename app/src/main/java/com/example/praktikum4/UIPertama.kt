589952657f9d17503a2544a8a2b8b212aba5e369
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-10-23 09:08:12 +0700

    menambahkan layout box di dalam layout kolom awal

--- app/src/main/java/com/example/praktikum4/UIPertama.kt
+++ app/src/main/java/com/example/praktikum4/UIPertama.kt
@@ -1,6 +1,7 @@
 package com.example.praktikum4
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -66,6 +67,12 @@ fun ActivitasPertama(modifier: Modifier){
                     )
                 }
             }
+        }
+        Box(
+            modifier= Modifier
+            .fillMaxSize()
+        ) {
+
         }
 
     }
