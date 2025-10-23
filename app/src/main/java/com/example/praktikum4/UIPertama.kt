7364dddb0097ddf4ff392cc98ef4609e3dd2de5c
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-10-23 08:19:56 +0700

    menambahkan res dan menambahkan gambar terus lanjut membuat fungsi ActivitasPertama dengan parameter modifier, dan menambahkan layout column pada fungsi tersebut dengan padd 100.dp

---
+++ app/src/main/java/com/example/praktikum4/UIPertama.kt
@@ -0,0 +1,14 @@
+package com.example.praktikum4
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun ActivitasPertama(modifier: Modifier){
+    Column(modifier= Modifier.padding(top = 100.dp)) {  }
+}
+
