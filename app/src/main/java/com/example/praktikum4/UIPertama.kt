1ac0cae56abfe45004bcf8339a9a6449a3a1a9c6
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-10-23 08:50:03 +0700

    menambahkan variabel gambar dengan mengarahkan ke tempat gambar itu tersimpan

--- app/src/main/java/com/example/praktikum4/UIPertama.kt
+++ app/src/main/java/com/example/praktikum4/UIPertama.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -39,7 +40,9 @@ fun ActivitasPertama(modifier: Modifier){
             colors = CardDefaults.cardColors(
                 containerColor = Color.DarkGray
             )){
-            Row (){  }
+            Row (){
+                val gambar = painterResource(id = R.drawable.logo_umy)
+            }
         }
 
     }
