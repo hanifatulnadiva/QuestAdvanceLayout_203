518544025498b7ff3cbb76d9e571034e1cc6c210
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-10-23 08:23:03 +0700

    menambahkan rata tengah  untuk mengatur teks agar nanti berada di tengah

--- app/src/main/java/com/example/praktikum4/UIPertama.kt
+++ app/src/main/java/com/example/praktikum4/UIPertama.kt
@@ -1,14 +1,18 @@
 package com.example.praktikum4
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 
 @Composable
 fun ActivitasPertama(modifier: Modifier){
-    Column(modifier= Modifier.padding(top = 100.dp)) {  }
+    Column(modifier= Modifier.padding(top = 100.dp)
+        .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally) {  }
 }
 
