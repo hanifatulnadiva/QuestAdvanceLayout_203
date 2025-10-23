753d96bdd144e413b7c1a21cb7bd77d655ed107f
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-10-23 08:46:40 +0700

    menambahkan warna untuk kontainer card dengan warnak dark

--- app/src/main/java/com/example/praktikum4/UIPertama.kt
+++ app/src/main/java/com/example/praktikum4/UIPertama.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -31,7 +34,10 @@ fun ActivitasPertama(modifier: Modifier){
         Spacer( modifier= Modifier.height(height = 25.dp))
         Card (modifier= Modifier
             .fillMaxWidth(fraction = 1f)
-            .padding(all = 12.dp)){  }
+            .padding(all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )){  }
 
     }
 }
